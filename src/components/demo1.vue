4701bf1efa42d9979cc13c6d2e843b906e0997a3
fengzheng <1924168021@qq.com>
2023-06-03 15:39:45 +0800

    仓库初始化

---
+++ src/components/demo1.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="demo1-box">
+
+    </div>
+</template>
+<script>
+export default {
+    name:'user-demo1',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/demo2.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="demo2-box">
+
+    </div>
+</template>
+<script>
+export default {
+    name:'user-demo2',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
